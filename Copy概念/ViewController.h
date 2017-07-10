349ca92dde710ba1cae7a62311db1d5c1d9e2060
liufeng0504 <1483682940@qq.com>
2017-07-10 14:35:15 +0800

    11

---
+++ Copy概念/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Copy概念
+//
+//  Created by 刘丰 on 2017/7/9.
+//  Copyright © 2017年 liufeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Copy概念/ViewController.m
@@ -0,0 +1,54 @@
+//
+//  ViewController.m
+//  Copy概念
+//
+//  Created by 刘丰 on 2017/7/9.
+//  Copyright © 2017年 liufeng. All rights reserved.
+//  copy => 不可变
+//  mutableCopy => 不可变
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    NSMutableString *str = [NSMutableString string];
+    [str appendString:@"liufeng"];
+    NSLog(@"%@---%p---%p", str, str, &str);
+    
+    NSString *cStr = str.copy;
+    NSLog(@"%@---%p---%p", cStr, cStr, &cStr);
+    
+    NSMutableString *mStr = str.mutableCopy;
+    NSLog(@"%@---%p---%p", mStr, mStr, &mStr);
+    
+    [str appendString:@" xihuan chenbei"];
+    NSLog(@"%@---%@---%@", str, cStr, mStr);
+}
+
+- (void)test
+{
+    NSString *str = @"liufeng";
+    NSLog(@"%@---%p---%p", str, str, &str);
+    
+    NSString *cStr = str.copy;
+    NSLog(@"%@---%p---%p", cStr, cStr, &cStr);
+    
+    NSMutableString *mStr = str.mutableCopy;
+    NSLog(@"%@---%p---%p", mStr, mStr, &mStr);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
